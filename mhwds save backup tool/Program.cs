0ad282bb7c8fe9d8eca959c698caaca7b0def8bb
Bruhamut <bbmaster358@gmail.com>
2025-08-12 18:19:57 -0500

    Reworked main backup function; now has functionality to copy save data .bin files into the zip file

--- mhwds save backup tool/Program.cs
+++ mhwds save backup tool/Program.cs
@@ -5,40 +5,45 @@ using System.Diagnostics;
 
 namespace mhwds_save_backup_tool;
 
+ //2246340
 internal static class Program
 {
+    private const string gameID = "2246340";
+    private const string userID = "null";
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
 
-    public static void CreateZipFileWithFiles(string zipPath, string filePath)
+   
+    public static void CreateZipBackup(string zipPath, string savePath)
     {
-        if (!File.Exists(filePath))
-        {
-            File.WriteAllText(filePath, "This is a file");
-        }
+        // Create zip file and update with files to be copied
         using (FileStream ZipFile = new FileStream(zipPath, FileMode.Create))
         using (ZipArchive archive = new ZipArchive(ZipFile, ZipArchiveMode.Update))
-            {
-            string entryName = Path.GetFileName(filePath);
-          
-            var existingEntry = archive.GetEntry(entryName);
-            if (existingEntry != null)
-            {
-                existingEntry.Delete();
+        {
+            string[] files = Directory.GetFiles(savePath, "*.bin");
+            foreach (var file in files)
+            {   
+                var entry = archive.CreateEntry(Path.GetFileName(file));
+
+                using (var entryStream = entry.Open())
+                using (var fileStream = new FileStream(file, FileMode.Open))
+                {
+                    fileStream.CopyTo(entryStream);
+                }
+
             }
-              archive.CreateEntryFromFile(filePath, entryName);
         }
     }
 
     public static void Main()
     {
         DateTime currentDT = DateTime.Now;
-        string formattedDT = currentDT.ToString("MM-dd-yyyy HH_mm");
+        string formattedDT = currentDT.ToString("MM-dd-yyyy HH_mm_ss");
         string zipPath = @$"C:\Temp\{formattedDT}.zip";
-        string filePath = @"C:\Temp\testfile.txt";
+        string savePath = @$"C:\Program Files (x86)\Steam\userdata\{userID}\{gameID}\remote\win64_save";
 
-        CreateZipFileWithFiles(zipPath, filePath);
+        CreateZipBackup(zipPath, savePath);
     }  
 }
